d62c3a29cbc870742dd904c913c922b83b25d9d7
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-07-08 21:00:29 -0400

    2.6 auth

--- Recipes/urls.py
+++ Recipes/urls.py
@@ -1,10 +1,11 @@
+# Recipes/urls.py
 from django.urls import path
 from .views import home_view, RecipeListView, RecipeDetailView
 
-app_name = 'recipes'
+app_name = 'recipes'  # Ensure this matches
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('recipes/', RecipeListView.as_view(), name='recipes-list'),
+    path('recipes/', RecipeListView.as_view(), name='recipes-list'),  # Name matches here
     path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='detail'),
 ]
--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -100,3 +100,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH
+LOGIN_URL='/login/'
